18594c6bfdf17f3554cb6ab22f08e64017faf48d
My name <me@exmple.com>
2025-01-23 15:57:54 +0100

    Add a check_reboot function

--- prueba1.py
+++ prueba1.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+import os 
+def check_reboot():
+    """Return True if the computere has a pending reboot."""
+    return os.path.exist("/run/reboot-requiered")
 def main():
     pass
 main()
+
 
     
