0437b5f34fca543472126ec767a07cd35238c656
a.remartinez <gayarri11@gmail.com>
2025-01-23 19:15:39 +0100

    Ramal para funcionalidad nueva de espacio en disco en reboot_Check.py

--- reboot_Check.py
+++ reboot_Check.py
@@ -1,23 +1,36 @@
-
+#!/usr/bin/env python3
 import os
-import sys
+import shutil
 
 def check_reboot():
     """Return True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
+def check_disk_space(disk="/"):
+    """
+    Return the percentage of free disk space.
+
+    Args:
+        disk (str): The path of the disk to check. Defaults to the root '/'.
+
+    Returns:
+        float: The percentage of free disk space.
+    """
+    usage = shutil.disk_usage(disk)
+    free_space_percentage = (usage.free / usage.total) * 100
+    return free_space_percentage
+
 def main():
     if check_reboot():
-        print("Atención : Reinicio pendiente.")
-        sys.exist(1)    
+        print("Reboot is required.")
     else:
-        print("Todo esta correcto.")
-        sys.exit(0)
-        
+        print("No reboot is required.")
+
+    disk_free = check_disk_space()
+    print(f"Free disk space: {disk_free:.2f}%")
+    if disk_free < 20:
+        print("Warning: Low disk space!")
 
 if __name__ == "__main__":
     main()
 
-
-
-    
